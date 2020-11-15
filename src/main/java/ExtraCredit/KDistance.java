e9f8fee9f1de2d45c76613ba29aaae47e2925c2a
cici1 <1249819124@qq.com>
2020-11-15 03:10:10 -0800

    homework5

--- src/main/java/ExtraCredit/KDistance.java
+++ src/main/java/ExtraCredit/KDistance.java
@@ -6,9 +6,64 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class KDistance {
-    public static List<Integer> distanceK(TreeNode<Integer> root, TreeNode<Integer> target, int k) {
-        // homework
-        List<Integer> result = new ArrayList<>();
-        return result;  // place holder
+    public static List<Integer> distanceK(TreeNode root, TreeNode target, int K) {
+        List<Integer> output = new ArrayList<>();
+        findTargetDepth(output, root, target, K, 0);
+        return output;
+    }
+
+    private static int findTargetDepth(List<Integer> output, TreeNode root, TreeNode target, int k, int depth) {
+        if (root == null) {
+            return -1;
+        }
+
+        if (root == target) {
+            // collect all nodes under target node within dist k
+            collectNodes(output, root, k);
+            return depth;
+        }
+
+        // search left
+        int targetDepth = findTargetDepth(output, root.left, target, k, depth + 1);
+        if (targetDepth != -1) {
+            // is current node at the dist K from target
+            if ((targetDepth - depth) == k) {
+                output.add((Integer) root.val);
+            } else {
+                // collect nodes on the right side
+                collectNodes(output, root.right, k - (targetDepth - depth) - 1);
+            }
+            return targetDepth;
+        }
+
+        // if not found, search right
+        targetDepth = findTargetDepth(output, root.right, target, k, depth + 1);
+        if (targetDepth != -1) {
+            // is current node at the dist K from target
+            if ((targetDepth - depth) == k) {
+                output.add((Integer) root.val);
+            } else {
+                // collect nodes on the left side
+                collectNodes(output, root.left, k - (targetDepth - depth) - 1);
+            }
+            return targetDepth;
+        }
+
+        // not found
+        return -1;
+    }
+
+    private static void collectNodes(List<Integer> output, TreeNode root, int k) {
+        if (root == null || k < 0) {
+            return;
+        }
+
+        if (k-- == 0) {
+            output.add((Integer) root.val);
+            return;
+        }
+
+        collectNodes(output, root.left, k);
+        collectNodes(output, root.right, k);
     }
 }
--- src/main/java/Problem3/InsertInBST.java
+++ src/main/java/Problem3/InsertInBST.java
@@ -3,7 +3,17 @@ package Problem3;
 import Problem1.TreeNode;
 
 public class InsertInBST {
-    public static void insert(TreeNode<Integer> root, int valToInsert) {
-        // homework
+    public static TreeNode<Integer> insert(TreeNode<Integer> root, int valToInsert) {
+        if (root == null)
+            return new TreeNode(valToInsert);
+
+
+        if (valToInsert > root.val)
+            root.right = insert(root.right, valToInsert);
+
+        else
+            root.left = insert(root.left, valToInsert);
+        return root;
+
     }
 }
